1049fead33f119ccb81cbc4ef0874c9dbbfc91a1
julius1329 <72988567+julius1329@users.noreply.github.com>
2021-03-21 01:29:43 +0100

    1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Borealer Nadelwald</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div class="navbar">
+      <a href="#funktionen">Funktionen</a>
+      <a href="#Vorraussetzung">Vorrausetzungen</a>
+    </div>
+    <div class="logo">
+      <a href="#" class="logo">Taiga</a>
+    </div>
+    <div id="funktionen">
+<p>funktionen</p>
+<p>
+  Regulierung des globalen Wasserhaushaltes außerdem filtert der Wald die verschmutzte Luft und gibt ihr mit Sauerstoff dabei speichert es automatisch Süßwasser und verhindert Bodenerosion.
+
+  Also Stabilisieren die borealen Nadelwälder das globale ökologische Gleichgewicht.
+</p>
+</div>
+<div class="picture2">
+  <img src="2picture.jpg" alt="" width="30%">
+</div>
+<div id="Vorraussetzung">
+<p>Vorraussetzung</p>
+<p>
+  Zum einem das der Boden sehr nährstoffarm ist und deshalb nur wenige Pflanzenarten zu finden sind.
